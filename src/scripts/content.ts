c5d3395b86de535369283829e7fc27e3d6e33178
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2023-12-29 12:30:11 +0100

    implement shared views collapsing

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,21 +1,16 @@
-const sharedViewsButtonSelector: string = 'button[data-garden-id="accordions.button"][id=":r0:--trigger:0"]';
+const sharedViewsButtonSelector: string = 'button[data-garden-id="accordions.button"][id=":r0:--trigger:0"][aria-expanded="true"]';
 
 const observer = new MutationObserver((mutations) => {
   mutations.forEach(() => {
+    const button: HTMLButtonElement | null = document.querySelector(sharedViewsButtonSelector);
+    if (button == null) {
+      console.error("Zendesk Shared Views Collapser extension failed to find the shared views button.");
+      return;
+    }
     if (document.querySelector(sharedViewsButtonSelector)) {
       observer.disconnect();
-      updateElement();
+      button.click();
     }
   });
 });
 observer.observe(document.body, { childList: true, subtree: true });
-
-async function updateElement() {
-  const button = document.querySelector(sharedViewsButtonSelector);
-  if (button == null || button.textContent == null) {
-    return;
-  }
-  if (button.textContent.includes("Shared")) {
-    button.textContent = "Shared (hi from the extension)";
-  }
-}
