336c818b08976ed198160d9786baea250138d4c6
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-05-12 21:28:04 +0200

    add popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import githubLogo from './github-mark.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Zendesk Tweaker</h1>
+            </header>
+            <p>Keep Zendesk shared views collapsed.</p>
+            <img src={githubLogo} className="App-github-logo" alt="logo"/>
+            <a href="https://github.com/tolache/zd-tweaker">tolache/zd-tweaker</a>
+        </div>
+    );
 }
 
 export default App;
