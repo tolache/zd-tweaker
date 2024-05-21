f36e3e16b0349189e74bcd3e2a092637c295293d
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-05-21 19:37:55 +0200

    update code style

--- prebuild.js
+++ prebuild.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 // Read package.json and get the 'version'
-let packageData = fs.readFileSync('package.json');
+let packageData = fs.readFileSync("package.json");
 let version = JSON.parse(packageData)['version'];
 
-let manifestData = fs.readFileSync('public/manifest.json');
+let manifestData = fs.readFileSync("public/manifest.json");
 let manifestJson = JSON.parse(manifestData);
-manifestJson['version'] = version;
+manifestJson["version"] = version;
 
 // Write updated JSON object back to manifest.json
-fs.writeFileSync('public/manifest.json', JSON.stringify(manifestJson, null, 2));
+fs.writeFileSync("public/manifest.json", JSON.stringify(manifestJson, null, 2));
