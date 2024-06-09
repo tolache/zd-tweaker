6fb2778aec3f48c8621748f41fe00240dfc0df8e
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-06-09 17:40:49 +0200

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders pop-up header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Zendesk Tweaker/i);
+  expect(headerElement).toBeInTheDocument();
 });
