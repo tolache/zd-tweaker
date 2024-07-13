74b3e64779c1f005c7ecd30fcbbb67d930e8bc84
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-07-13 12:18:34 +0200

    refactor

--- src/content-scripts/ticket.ts
+++ src/content-scripts/ticket.ts
@@ -7,72 +7,9 @@ const observer = new MutationObserver(() => {
     const infoBars: NodeListOf<HTMLDivElement> | null = document.querySelectorAll(infoBarSelector);
     if (infoBars.length > 0) {
         infoBars.forEach((infoBar) => {
-            if (infoBar.textContent?.trim().startsWith("Shared with") || infoBar.textContent?.trim().startsWith("This is an archived ticket")) {
-                let infoBarParent = infoBar.parentNode;
-                if (infoBarParent !== null) {
-                    const infoDiv: HTMLDivElement = infoBarParent as HTMLDivElement;
-                    infoDiv.style.display = "none";
-                }
-            }
-
-            if (infoBar.textContent?.trim().startsWith("Follow-up") && infoBar.firstChild?.childNodes) {
-                const navbarTicketButtonSelector: string = "span.ember-view.btn[data-tracking-id='tabs-section-nav-item-ticket']";
-                const followUpsButtonSelector: string = ".zd-tweaker.follow-ups";
-                const parentTicketButtonSelector: string = ".zd-tweaker.parent-ticket";
-                const navbarTicketButton = document.querySelector(navbarTicketButtonSelector);
-
-                if (navbarTicketButton?.parentNode) {
-                    const followUpsButtonAlreadyAdded = navbarTicketButton?.parentNode.querySelector(followUpsButtonSelector);
-                    const parentTicketButtonAlreadyAdded = navbarTicketButton?.parentNode.querySelector(parentTicketButtonSelector);
-
-                    const linkedTicketsButton: HTMLSpanElement = document.createElement("span");
-                    linkedTicketsButton.classList.add("btn");
-                    linkedTicketsButton.classList.add("zd-tweaker");
-
-                    const infoBarIsFollowUps: boolean = infoBar.textContent?.trim().startsWith("Follow-ups");
-                    if (infoBarIsFollowUps && !followUpsButtonAlreadyAdded) {
-                        linkedTicketsButton.classList.add("follow-ups");
-
-                        const followUpsButtonArrow = document.createElement("small");
-                        followUpsButtonArrow.innerText = "❯ ";
-                        linkedTicketsButton.appendChild(followUpsButtonArrow);
-
-                        const ticketLinksAndSeparators = (infoBar.firstChild as HTMLElement).querySelector("strong")?.childNodes;
-                        if (ticketLinksAndSeparators) {
-                            for (const ticketLinkOrSeparator of Array.from(ticketLinksAndSeparators)) {
-                                linkedTicketsButton.appendChild(ticketLinkOrSeparator);
-                            }
-                        }
-
-                        if (!navbarTicketButton?.parentNode.querySelector(".zd-tweaker.follow-ups")) {
-                            navbarTicketButton.parentNode.insertBefore(linkedTicketsButton, navbarTicketButton.nextSibling);
-                        }
-                    }
-
-                    const infoBarIsParentTicket: boolean = infoBar.textContent?.trim().startsWith("Follow-up to ticket");
-                    if (infoBarIsParentTicket && !parentTicketButtonAlreadyAdded) {
-                        linkedTicketsButton.classList.add("parent-ticket");
-                        const ticketLinksAndSeparators = (infoBar.firstChild as HTMLElement).querySelector("strong")?.childNodes;
-                        if (ticketLinksAndSeparators) {
-                            for (const ticketLinkOrSeparator of Array.from(ticketLinksAndSeparators)) {
-                                linkedTicketsButton.appendChild(ticketLinkOrSeparator);
-                            }
-                        }
-
-                        const parentTicketButtonArrow = document.createElement("small");
-                        parentTicketButtonArrow.innerText = " ❮";
-                        linkedTicketsButton.appendChild(parentTicketButtonArrow);
-
-                        navbarTicketButton.parentNode.insertBefore(linkedTicketsButton, navbarTicketButton);
-                    }
-
-                    let infoBarParent = infoBar.parentNode;
-                    if (infoBarParent !== null) {
-                        const infoDiv: HTMLDivElement = infoBarParent as HTMLDivElement;
-                        infoDiv.style.display = "none";
-                    }
-                }
-            }
+            hideSharedTicketInfoBar(infoBar);
+            hideArchivedTicketInfoBar(infoBar);
+            hideFollowUpInfoBars(infoBar);
         });
     }
 });
@@ -83,3 +20,80 @@ const observer = new MutationObserver(() => {
         observer.observe(document, {attributes: true, subtree: true});
     }
 })();
+
+function hideSharedTicketInfoBar(infoBar: HTMLDivElement) {
+    if (infoBar.textContent?.trim().startsWith("Shared with")) {
+        hideInfoBarParentNode(infoBar);
+    }
+}
+
+function hideArchivedTicketInfoBar(infoBar: HTMLDivElement) {
+    if (infoBar.textContent?.trim().startsWith("This is an archived ticket")) {
+        hideInfoBarParentNode(infoBar);
+    }
+}
+
+function hideFollowUpInfoBars(infoBar: HTMLDivElement) {
+    if (infoBar.textContent?.trim().startsWith("Follow-up") && infoBar.firstChild?.childNodes) {
+        const navbarTicketButtonSelector: string = "span.ember-view.btn[data-tracking-id='tabs-section-nav-item-ticket']";
+        const followUpsButtonSelector: string = ".zd-tweaker.follow-ups";
+        const parentTicketButtonSelector: string = ".zd-tweaker.parent-ticket";
+        const navbarTicketButton = document.querySelector(navbarTicketButtonSelector);
+
+        if (navbarTicketButton?.parentNode) {
+            const followUpsButtonAlreadyAdded = navbarTicketButton?.parentNode.querySelector(followUpsButtonSelector);
+            const parentTicketButtonAlreadyAdded = navbarTicketButton?.parentNode.querySelector(parentTicketButtonSelector);
+
+            const linkedTicketsButton: HTMLSpanElement = document.createElement("span");
+            linkedTicketsButton.classList.add("btn");
+            linkedTicketsButton.classList.add("zd-tweaker");
+
+            const infoBarIsFollowUps: boolean = infoBar.textContent?.trim().startsWith("Follow-ups");
+            if (infoBarIsFollowUps && !followUpsButtonAlreadyAdded) {
+                linkedTicketsButton.classList.add("follow-ups");
+
+                const followUpsButtonArrow = document.createElement("small");
+                followUpsButtonArrow.innerText = "❯ ";
+                linkedTicketsButton.appendChild(followUpsButtonArrow);
+
+                const ticketLinksAndSeparators = (infoBar.firstChild as HTMLElement).querySelector("strong")?.childNodes;
+                if (ticketLinksAndSeparators) {
+                    for (const ticketLinkOrSeparator of Array.from(ticketLinksAndSeparators)) {
+                        linkedTicketsButton.appendChild(ticketLinkOrSeparator);
+                    }
+                }
+
+                if (!navbarTicketButton?.parentNode.querySelector(".zd-tweaker.follow-ups")) {
+                    navbarTicketButton.parentNode.insertBefore(linkedTicketsButton, navbarTicketButton.nextSibling);
+                }
+            }
+
+            const infoBarIsParentTicket: boolean = infoBar.textContent?.trim().startsWith("Follow-up to ticket");
+            if (infoBarIsParentTicket && !parentTicketButtonAlreadyAdded) {
+                linkedTicketsButton.classList.add("parent-ticket");
+                const ticketLinksAndSeparators = (infoBar.firstChild as HTMLElement).querySelector("strong")?.childNodes;
+                if (ticketLinksAndSeparators) {
+                    for (const ticketLinkOrSeparator of Array.from(ticketLinksAndSeparators)) {
+                        linkedTicketsButton.appendChild(ticketLinkOrSeparator);
+                    }
+                }
+
+                const parentTicketButtonArrow = document.createElement("small");
+                parentTicketButtonArrow.innerText = " ❮";
+                linkedTicketsButton.appendChild(parentTicketButtonArrow);
+
+                navbarTicketButton.parentNode.insertBefore(linkedTicketsButton, navbarTicketButton);
+            }
+
+            hideInfoBarParentNode(infoBar);
+        }
+    }
+}
+
+function hideInfoBarParentNode(infoBar: HTMLDivElement) {
+    let infoBarParent = infoBar.parentNode;
+    if (infoBarParent !== null) {
+        const infoDiv: HTMLDivElement = infoBarParent as HTMLDivElement;
+        infoDiv.style.display = "none";
+    }
+}
